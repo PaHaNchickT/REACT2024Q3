6888eeb5bc53979c528eb3b85e612b55e3580890
Pavel <you@example.com>
2024-07-07 21:58:38 +0300

    refactor: delete commented code lines

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,3 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
 import { Component, ReactNode } from 'react'
 import Search from './components/search/search'
 import Results from './components/results/results'
