10b08e024ec69042a1342db8fdd91f827a158b36
Pavel <you@example.com>
2024-07-11 00:54:09 +0300

    fix: add catch block in API module

--- src/components/utils/API.tsx
+++ src/components/utils/API.tsx
@@ -7,9 +7,13 @@ export default class API {
         'X-API-KEY': 'fe77bc0c-1287-4d70-adb2-d5f3b64ee3e7',
         'Content-Type': 'application/json',
       },
-    }).then((resp) => {
-      data = resp.json()
     })
+      .then((resp) => {
+        data = resp.json()
+      })
+      .catch((err) => {
+        throw new Error(err.text)
+      })
 
     return data
   }
@@ -22,9 +26,13 @@ export default class API {
         'X-API-KEY': 'fe77bc0c-1287-4d70-adb2-d5f3b64ee3e7',
         'Content-Type': 'application/json',
       },
-    }).then((resp) => {
-      data = resp.json()
     })
+      .then((resp) => {
+        data = resp.json()
+      })
+      .catch((err) => {
+        throw new Error(err.text)
+      })
 
     return data
   }
@@ -33,9 +41,13 @@ export default class API {
     let data
     await fetch('https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=TOP_250_MOVIES&page=1', {
       method: 'GET',
-    }).then((resp) => {
-      data = resp.json()
     })
+      .then((resp) => {
+        data = resp.json()
+      })
+      .catch((err) => {
+        throw new Error(err.text)
+      })
 
     return data
   }
