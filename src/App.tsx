52d907c345dd091de6c0b8fa556a18f460e61987
Pavel <you@example.com>
2024-07-11 02:55:15 +0300

    feat: migrate app.tsx from class to function

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react'
+import { SetStateAction, useEffect, useState } from 'react'
 import { Search } from './components/search/search'
 import { Results } from './components/results/results'
 import { FilmObj } from './components/types'
@@ -8,64 +8,48 @@ import ErrorBoundary from './components/error-boundary/errorBoundary'
 import { CLASS_NAMES, TEXT_CONTENT } from './components/constants'
 import { Loader } from './components/loader/loader'
 
-class App extends Component<Record<string, string>, { value: FilmObj[]; isLoading: boolean }> {
-  API = API()
+export function App() {
+  const [value, setValue] = useState([])
+  const [isLoading, setLoading] = useState(true)
 
-  state = {
-    value: [],
-    isLoading: true,
-  }
-
-  constructor(props: Record<string, string>) {
-    super(props)
-
-    this.buttonHandler = this.buttonHandler.bind(this)
-  }
-
-  async buttonHandler(value: string) {
-    this.setState({ isLoading: true })
+  const buttonHandler = async (value: string) => {
+    setLoading(true)
 
     const searchString = value.trim()
-    let temp = []
+    let tempValue = []
     if (searchString === TEXT_CONTENT.errorID) {
-      temp = ((await this.API.fakeRequest()) as unknown as { items: FilmObj[] }).items
+      tempValue = ((await API().fakeRequest()) as unknown as { items: FilmObj[] }).items
     } else if (searchString === '') {
-      temp = ((await this.API.start()) as unknown as { items: FilmObj[] }).items
+      tempValue = ((await API().start()) as unknown as { items: FilmObj[] }).items
     } else {
-      temp = ((await this.API.search(searchString)) as unknown as { films: FilmObj[] }).films
+      tempValue = ((await API().search(searchString)) as unknown as { films: FilmObj[] }).films
     }
 
-    this.setState({ isLoading: false })
-    this.setState({ value: temp })
+    setLoading(false)
+    setValue(tempValue as SetStateAction<never[]>)
   }
 
-  componentDidMount() {
-    this.buttonHandler(LocalStorage().getValue())
-  }
+  useEffect(() => {
+    buttonHandler(LocalStorage().getValue())
+  }, [])
 
-  render() {
-    let resultsUI = <Results value={this.state.value} />
-    if (this.state.isLoading) {
-      resultsUI = <Loader />
-    }
+  let resultsUI = <Results value={value} />
+  if (isLoading) resultsUI = <Loader />
 
-    return (
-      <ErrorBoundary
-        fallback={
-          <div className={CLASS_NAMES.errorWrapper}>
-            <div className={CLASS_NAMES.errorCont}>
-              <h2>{TEXT_CONTENT.errorTitle}</h2>
-              <div></div>
-              <p>{TEXT_CONTENT.errorText}</p>
-            </div>
+  return (
+    <ErrorBoundary
+      fallback={
+        <div className={CLASS_NAMES.errorWrapper}>
+          <div className={CLASS_NAMES.errorCont}>
+            <h2>{TEXT_CONTENT.errorTitle}</h2>
+            <div></div>
+            <p>{TEXT_CONTENT.errorText}</p>
           </div>
-        }
-      >
-        <Search onClick={this.buttonHandler} />
-        {resultsUI}
-      </ErrorBoundary>
-    )
-  }
+        </div>
+      }
+    >
+      <Search onClick={buttonHandler} />
+      {resultsUI}
+    </ErrorBoundary>
+  )
 }
-
-export default App
