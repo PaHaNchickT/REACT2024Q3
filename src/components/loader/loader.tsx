43337eb8e67e433023b3827d06211d86b42b9d35
Pavel <you@example.com>
2024-07-11 00:50:04 +0300

    refactor: delete types in loader module

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -1,9 +1,9 @@
-import { Component, ReactNode } from 'react'
+import { Component } from 'react'
 import { CLASS_NAMES } from '../constants'
 
 import './loader.css'
 
-export default class Loader extends Component<{ [key: string]: ReactNode }, { [key: string]: boolean }> {
+export default class Loader extends Component {
   render() {
     return (
       <div className={CLASS_NAMES.loaderWrapper}>
