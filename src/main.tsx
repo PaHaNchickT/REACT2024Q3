a5c8e5ed05c554003abd4facbd2f298aa7c0b648
Pavel <you@example.com>
2024-07-11 16:14:02 +0300

    feat: add root routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { App } from './App.tsx'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+])
 
 const root = document.getElementById('root')
 if (root) {
   ReactDOM.createRoot(root).render(
     <React.StrictMode>
-      <App />
+      <RouterProvider router={router} />
     </React.StrictMode>
   )
 }
