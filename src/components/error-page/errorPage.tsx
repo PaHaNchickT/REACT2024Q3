bc4232befc260a2b2976f794930cf100cf777437
Pavel <you@example.com>
2024-07-20 00:51:52 +0300

    fix: update errorPage component

--- src/components/error-page/errorPage.tsx
+++ src/components/error-page/errorPage.tsx
@@ -1,25 +1,18 @@
 import { Link, useRouteError } from 'react-router-dom'
 import { TEXT_CONTENT } from '../constants'
 import './errorPage.css'
-import { useEffect } from 'react'
-import { useLocalStorage } from '../../hooks/useLocalStorage'
 
 export function ErrorPage() {
-  const [savedValue, setSavedValue] = useLocalStorage('')
   const error = useRouteError()
-  console.log(error)
-
-  useEffect(() => {
-    setSavedValue('')
-  }, [])
+  if (error) console.log(error)
 
   return (
-    <div className="error-page__wrapper" key={savedValue}>
+    <div className="error-page__wrapper">
       <div className="error-page__cont">
         <h2>{TEXT_CONTENT.errorPageTitle}</h2>
         <div></div>
         <p>{TEXT_CONTENT.errorPageText}</p>
-        <Link to="/1">{TEXT_CONTENT.btnErrorHome}</Link>
+        <Link to="/">{TEXT_CONTENT.btnErrorHome}</Link>
       </div>
     </div>
   )
