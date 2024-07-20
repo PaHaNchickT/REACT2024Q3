3759d4f5d559c81852b1b4aead77f60db7557094
Pavel <you@example.com>
2024-07-20 18:47:11 +0300

    feat: add light/dark theme for error page

--- src/components/error-page/errorPage.tsx
+++ src/components/error-page/errorPage.tsx
@@ -1,8 +1,12 @@
 import { Link, useRouteError } from 'react-router-dom'
 import { TEXT_CONTENT } from '../constants'
 import './errorPage.css'
+import { useContext } from 'react'
+import { ThemeContext } from '../../App'
 
 export function ErrorPage() {
+  const { theme } = useContext(ThemeContext)
+
   const error = useRouteError()
   if (error) console.log(error)
 
@@ -12,7 +16,9 @@ export function ErrorPage() {
         <h2>{TEXT_CONTENT.errorPageTitle}</h2>
         <div></div>
         <p>{TEXT_CONTENT.errorPageText}</p>
-        <Link to="/">{TEXT_CONTENT.btnErrorHome}</Link>
+        <Link to="/" className={theme}>
+          {TEXT_CONTENT.btnErrorHome}
+        </Link>
       </div>
     </div>
   )
