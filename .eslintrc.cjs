45298139358dfde70199a05dfc9b62b8945c60e3
Pavel <you@example.com>
2024-07-25 19:52:44 +0300

    doc: turn off esling refresh rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,5 +8,6 @@ module.exports = {
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     'react-hooks/exhaustive-deps': 0,
+    'react-refresh/only-export-components': 0,
   },
 }
