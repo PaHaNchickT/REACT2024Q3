c6a141bdf864f8bbc10da7a45741aced9e74b0fc
Pavel <you@example.com>
2024-07-30 18:54:34 +0300

    doc: delete unused resultsSlice

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,13 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import searchDataSliceReducer from './searchSlice'
-import resultsDataSliceReducer from './resultsSlice'
 import detailsDataSliceReducer from './detailsSlice'
 import selectedDataSliceReducer from './selectedSlice'
 
 export default configureStore({
   reducer: {
     searchData: searchDataSliceReducer,
-    resultsData: resultsDataSliceReducer,
     detailsData: detailsDataSliceReducer,
     selectedData: selectedDataSliceReducer,
   },
