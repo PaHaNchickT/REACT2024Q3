ef6f99b4525ffef0535140570d197755d4f101c8
Pavel <you@example.com>
2024-08-04 23:26:26 +0300

    doc: edit nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export', // Outputs a Single-Page Application (SPA).
-  distDir: './dist', // Changes the build output directory to `./dist/`.
+  distDir: './dist',
+  compiler: {
+    styledComponents: true,
+  },
 }
 
 export default nextConfig
