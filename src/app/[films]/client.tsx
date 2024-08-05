5a23d9f25deffa62c7cc21135c804cc71ed562b7
Pavel <you@example.com>
2024-08-05 21:32:45 +0300

    fix: fix 'Prop `className` did not match' error

--- src/app/[films]/client.tsx
+++ src/app/[films]/client.tsx
@@ -2,11 +2,17 @@
 
 import { useEffect } from 'react'
 import { Search } from '../../components/search/search'
-import { Results } from '../../components/results/results'
 import { Provider, useDispatch, useSelector } from 'react-redux'
 import store from '../../services/store'
 import { FilmResp, reduxStore } from '../../components/types'
 import { setTheme } from '../../services/themeSlice'
+import dynamic from 'next/dynamic'
+import { Loader } from '../../components/loader/loader'
+
+const Results = dynamic(() => import('../../components/results/results').then((module) => module.Results), {
+  ssr: false,
+  loading: () => <Loader theme="default" />,
+})
 
 export default function App({ results }: { results: FilmResp }) {
   const dispatch = useDispatch()
