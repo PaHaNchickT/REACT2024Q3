6a30aaad7e03f63fee4e6775832b410ba86c8aa6
Pavel <you@example.com>
2024-08-06 16:42:21 +0300

    fix: replace a with Link in pagination

--- src/components/pagination/pagination.tsx
+++ src/components/pagination/pagination.tsx
@@ -1,32 +1,26 @@
-import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { usePathname, useSearchParams } from 'next/navigation'
 import { useSelector } from 'react-redux'
 import { reduxStore } from '../types'
+import Link from 'next/link'
 
 export function Pagination(props: { page: number; currentPage: number }) {
   const theme = useSelector((state: reduxStore) => state.themeData.themeData)
-  const router = useRouter()
   const pathname = usePathname()
   const searchParams = useSearchParams()
 
   let buttonClassname = 'pendingBtn'
   if (props.page === +props.currentPage) buttonClassname = 'activeBtn'
 
-  const onClick = () => {
-    router.push(
-      `${pathname}?page=${props.page}${(searchParams.get('search') && `&search=${searchParams.get('search')}`) || ''}`
-    )
-  }
-
   return (
     <li>
-      <a
-        onClick={onClick}
+      <Link
+        href={`${pathname}?page=${props.page}${(searchParams.get('search') && `&search=${searchParams.get('search')}`) || ''}`}
         id={props.page.toString()}
         className={`${buttonClassname} ${theme.color}`}
         data-testid="pendingBtn"
       >
         {props.page}
-      </a>
+      </Link>
     </li>
   )
 }
