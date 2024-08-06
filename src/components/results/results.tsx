c4b6faf32c40e11cd19243928318367134ca6d53
Pavel <you@example.com>
2024-08-06 20:17:53 +0300

    fix: fix bug with empty checkboxes while items selected

--- src/components/results/results.tsx
+++ src/components/results/results.tsx
@@ -11,12 +11,13 @@ import { addItemData, removeItemData } from '../../services/selectedSlice'
 import { Selected } from '../selected-panel/selected-info'
 import { useSearchParams, useRouter } from 'next/navigation'
 import ErrorPage from '../../pages/404'
+import { itemsToArray } from '../../utils/itemsToArray'
 
 export function Results({ data }: { data: { results: FilmResp; details?: FilmInfo } }) {
-  const selectedItems: number[] = []
+  const theme = useSelector((state: reduxStore) => state.themeData.themeData)
   const detailsData = useSelector((state: reduxStore) => state.detailsData.detailsData)
   const selectedData = useSelector((state: reduxStore) => state.selectedData.selectedData)
-  const theme = useSelector((state: reduxStore) => state.themeData.themeData)
+  const selectedItems: number[] = itemsToArray(selectedData.selectedItems)
 
   const router = useRouter()
   const dispatch = useDispatch()
