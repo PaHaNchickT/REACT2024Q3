88fc0a3766cb5240e27af4dbd77f0867daa9817e
Pavel <you@example.com>
2024-08-08 17:30:09 +0300

    feat: update redux tests

--- src/__ tests __/Redux.test.tsx
+++ src/__ tests __/Redux.test.tsx
@@ -1,235 +1,208 @@
-import '@testing-library/jest-dom'
-// import searchDataSliceReducer, { setSearchValue, setPage } from '../services/searchSlice'
-// import resultsDataSliceReducer, { setResultsData } from '../services/resultsSlice'
-// import detailsDataSliceReducer, { setIsClosed, setFilmData } from '../services/detailsSlice'
-// import selectedDataSliceReducer, { addItemData, removeItemData, clearItemData } from '../services/selectedSlice'
+import detailsDataSliceReducer, { setIsClosed, setFilmData } from '../services/detailsSlice'
+import selectedDataSliceReducer, { addItemData, removeItemData, clearItemData } from '../services/selectedSlice'
+import themeDataSliceReducer, { setTheme } from '../services/themeSlice'
 
-// describe('Redux searchDataSlice', () => {})
-// describe('Redux store', () => {
-//   it('should shange search value with "setSearchValue" action', () => {
-//     const action = { type: setSearchValue.type, payload: { value: 'test' } }
-//     const result = searchDataSliceReducer({ searchData: { value: '', page: 1 } }, action)
+describe('Redux detailsDataSlice', () => {
+  it('should shange isClosed and filmId values with "setIsClosed" action', () => {
+    const action = { type: setIsClosed.type, payload: { isClosed: true, filmId: null } }
+    const result = detailsDataSliceReducer(
+      {
+        detailsData: {
+          isClosed: false,
+          filmId: 0,
+          filmData: { kinopoiskId: 0 },
+        },
+      },
+      action
+    )
 
-//     expect(result.searchData.value).toBe('test')
-//   })
+    expect(result.detailsData).toEqual({ isClosed: true, filmId: 0, filmData: { kinopoiskId: 0 } })
+  })
 
-//   it('should shange search page with "setPage" action', () => {
-//     const action = { type: setPage.type, payload: { page: 2 } }
-//     const result = searchDataSliceReducer({ searchData: { value: '', page: 1 } }, action)
+  it('should shange filmData value with "setFilmData" action', () => {
+    const action = { type: setFilmData.type, payload: { isClosed: true, filmId: 999, filmData: { kinopoiskId: 999 } } }
+    const result = detailsDataSliceReducer(
+      {
+        detailsData: {
+          isClosed: false,
+          filmId: 0,
+          filmData: { kinopoiskId: 0 },
+        },
+      },
+      action
+    )
 
-//     expect(result.searchData.page).toBe(2)
-//   })
-// })
+    expect(result.detailsData.filmData.kinopoiskId).toBe(999)
+  })
+})
 
-// describe('Redux resultsDataSlice', () => {
-//   it('should shange results data with "setResultsData" action', () => {
-//     const action = { type: setResultsData.type, payload: { items: [], total: 1, totalPages: 1 } }
-//     const result = resultsDataSliceReducer(
-//       {
-//         resultsData: {
-//           items: [],
-//           total: 0,
-//           totalPages: 0,
-//         },
-//       },
-//       action
-//     )
+describe('Redux selectedDataSlice', () => {
+  it('should add item data with "addItemData" action', () => {
+    const action = { type: addItemData.type, payload: { kinopoiskId: 999 } }
+    const result = selectedDataSliceReducer(
+      {
+        selectedData: {
+          selectedItems: [],
+        },
+      },
+      action
+    )
 
-//     expect(result.resultsData.total).toBe(1)
-//   })
-// })
+    expect(result.selectedData.selectedItems[0].kinopoiskId).toBe(999)
+  })
 
-// describe('Redux detailsDataSlice', () => {
-//   it('should shange isClosed and filmId values with "setIsClosed" action', () => {
-//     const action = { type: setIsClosed.type, payload: { isClosed: true, filmId: null } }
-//     const result = detailsDataSliceReducer(
-//       {
-//         detailsData: {
-//           isClosed: false,
-//           filmId: 0,
-//           filmData: { kinopoiskId: 0 },
-//         },
-//       },
-//       action
-//     )
+  it('should remove item data with "removeItemData" action', () => {
+    const action = {
+      type: removeItemData.type,
+      payload: {
+        kinopoiskId: 463634,
+        nameRu: 'Тест',
+        nameEn: 'Test',
+        nameOriginal: 'Testy Test',
+        countries: [
+          {
+            country: 'США',
+          },
+          {
+            country: 'Китай',
+          },
+        ],
+        genres: [
+          {
+            genre: 'триллер',
+          },
+          {
+            genre: 'фантастика',
+          },
+          {
+            genre: 'боевик',
+          },
+          {
+            genre: 'ужасы',
+          },
+        ],
+        rating: 999,
+        ratingKinopoisk: 999,
+        ratingImdb: 999,
+        year: 2014,
+        type: 'FILM',
+        posterUrl: 'https://kinopoiskapiunofficial.tech/images/posters/kp/463634.jpg',
+        posterUrlPreview: 'https://kinopoiskapiunofficial.tech/images/posters/kp_small/463634.jpg',
+      },
+    }
+    const result = selectedDataSliceReducer(
+      {
+        selectedData: {
+          selectedItems: [
+            {
+              kinopoiskId: 463634,
+              nameRu: 'Тест',
+              nameEn: 'Test',
+              nameOriginal: 'Testy Test',
+              countries: [
+                {
+                  country: 'США',
+                },
+                {
+                  country: 'Китай',
+                },
+              ],
+              genres: [
+                {
+                  genre: 'триллер',
+                },
+                {
+                  genre: 'фантастика',
+                },
+                {
+                  genre: 'боевик',
+                },
+                {
+                  genre: 'ужасы',
+                },
+              ],
+              rating: 999,
+              ratingKinopoisk: 999,
+              ratingImdb: 999,
+              year: 2014,
+              type: 'FILM',
+              posterUrl: 'https://kinopoiskapiunofficial.tech/images/posters/kp/463634.jpg',
+              posterUrlPreview: 'https://kinopoiskapiunofficial.tech/images/posters/kp_small/463634.jpg',
+            },
+          ],
+        },
+      },
+      action
+    )
 
-//     expect(result.detailsData).toEqual({ isClosed: true, filmId: 0, filmData: { kinopoiskId: 0 } })
-//   })
+    expect(result.selectedData.selectedItems).toHaveLength(0)
+  })
 
-//   it('should shange filmData value with "setFilmData" action', () => {
-//     const action = { type: setFilmData.type, payload: { isClosed: true, filmId: 999, filmData: { kinopoiskId: 999 } } }
-//     const result = detailsDataSliceReducer(
-//       {
-//         detailsData: {
-//           isClosed: false,
-//           filmId: 0,
-//           filmData: { kinopoiskId: 0 },
-//         },
-//       },
-//       action
-//     )
+  it('should clear items with "clearItemData" action', () => {
+    const action = {
+      type: clearItemData.type,
+    }
+    const result = selectedDataSliceReducer(
+      {
+        selectedData: {
+          selectedItems: [
+            {
+              kinopoiskId: 463634,
+              nameRu: 'Тест',
+              nameEn: 'Test',
+              nameOriginal: 'Testy Test',
+              countries: [
+                {
+                  country: 'США',
+                },
+                {
+                  country: 'Китай',
+                },
+              ],
+              genres: [
+                {
+                  genre: 'триллер',
+                },
+                {
+                  genre: 'фантастика',
+                },
+                {
+                  genre: 'боевик',
+                },
+                {
+                  genre: 'ужасы',
+                },
+              ],
+              rating: 999,
+              ratingKinopoisk: 999,
+              ratingImdb: 999,
+              year: 2014,
+              type: 'FILM',
+              posterUrl: 'https://kinopoiskapiunofficial.tech/images/posters/kp/463634.jpg',
+              posterUrlPreview: 'https://kinopoiskapiunofficial.tech/images/posters/kp_small/463634.jpg',
+            },
+          ],
+        },
+      },
+      action
+    )
 
-//     expect(result.detailsData.filmData.kinopoiskId).toBe(999)
-//   })
-// })
+    expect(result.selectedData.selectedItems).toHaveLength(0)
+  })
+})
 
-// describe('Redux selectedDataSlice', () => {
-//   it('should add item data with "addItemData" action', () => {
-//     const action = { type: addItemData.type, payload: { kinopoiskId: 999 } }
-//     const result = selectedDataSliceReducer(
-//       {
-//         selectedData: {
-//           selectedItems: [],
-//         },
-//       },
-//       action
-//     )
+describe('Redux themeDataSlice', () => {
+  it('should shange color with "setTheme" action', () => {
+    const action = { type: setTheme.type, payload: 'dark' }
+    const result = themeDataSliceReducer(
+      {
+        themeData: {
+          color: 'light',
+        },
+      },
+      action
+    )
 
-//     expect(result.selectedData.selectedItems[0].kinopoiskId).toBe(999)
-//   })
-
-//   it('should remove item data with "removeItemData" action', () => {
-//     const action = {
-//       type: removeItemData.type,
-//       payload: {
-//         kinopoiskId: 463634,
-//         nameRu: 'Тест',
-//         nameEn: 'Test',
-//         nameOriginal: 'Testy Test',
-//         countries: [
-//           {
-//             country: 'США',
-//           },
-//           {
-//             country: 'Китай',
-//           },
-//         ],
-//         genres: [
-//           {
-//             genre: 'триллер',
-//           },
-//           {
-//             genre: 'фантастика',
-//           },
-//           {
-//             genre: 'боевик',
-//           },
-//           {
-//             genre: 'ужасы',
-//           },
-//         ],
-//         rating: 999,
-//         ratingKinopoisk: 999,
-//         ratingImdb: 999,
-//         year: 2014,
-//         type: 'FILM',
-//         posterUrl: 'https://kinopoiskapiunofficial.tech/images/posters/kp/463634.jpg',
-//         posterUrlPreview: 'https://kinopoiskapiunofficial.tech/images/posters/kp_small/463634.jpg',
-//       },
-//     }
-//     const result = selectedDataSliceReducer(
-//       {
-//         selectedData: {
-//           selectedItems: [
-//             {
-//               kinopoiskId: 463634,
-//               nameRu: 'Тест',
-//               nameEn: 'Test',
-//               nameOriginal: 'Testy Test',
-//               countries: [
-//                 {
-//                   country: 'США',
-//                 },
-//                 {
-//                   country: 'Китай',
-//                 },
-//               ],
-//               genres: [
-//                 {
-//                   genre: 'триллер',
-//                 },
-//                 {
-//                   genre: 'фантастика',
-//                 },
-//                 {
-//                   genre: 'боевик',
-//                 },
-//                 {
-//                   genre: 'ужасы',
-//                 },
-//               ],
-//               rating: 999,
-//               ratingKinopoisk: 999,
-//               ratingImdb: 999,
-//               year: 2014,
-//               type: 'FILM',
-//               posterUrl: 'https://kinopoiskapiunofficial.tech/images/posters/kp/463634.jpg',
-//               posterUrlPreview: 'https://kinopoiskapiunofficial.tech/images/posters/kp_small/463634.jpg',
-//             },
-//           ],
-//         },
-//       },
-//       action
-//     )
-
-//     expect(result.selectedData.selectedItems).toHaveLength(0)
-//   })
-
-//   it('should clear items with "clearItemData" action', () => {
-//     const action = {
-//       type: clearItemData.type,
-//     }
-//     const result = selectedDataSliceReducer(
-//       {
-//         selectedData: {
-//           selectedItems: [
-//             {
-//               kinopoiskId: 463634,
-//               nameRu: 'Тест',
-//               nameEn: 'Test',
-//               nameOriginal: 'Testy Test',
-//               countries: [
-//                 {
-//                   country: 'США',
-//                 },
-//                 {
-//                   country: 'Китай',
-//                 },
-//               ],
-//               genres: [
-//                 {
-//                   genre: 'триллер',
-//                 },
-//                 {
-//                   genre: 'фантастика',
-//                 },
-//                 {
-//                   genre: 'боевик',
-//                 },
-//                 {
-//                   genre: 'ужасы',
-//                 },
-//               ],
-//               rating: 999,
-//               ratingKinopoisk: 999,
-//               ratingImdb: 999,
-//               year: 2014,
-//               type: 'FILM',
-//               posterUrl: 'https://kinopoiskapiunofficial.tech/images/posters/kp/463634.jpg',
-//               posterUrlPreview: 'https://kinopoiskapiunofficial.tech/images/posters/kp_small/463634.jpg',
-//             },
-//           ],
-//         },
-//       },
-//       action
-//     )
-
-//     expect(result.selectedData.selectedItems).toHaveLength(0)
-//   })
-// })
-
-describe('Placeholder', () => {
-  it('should change app color scheme after theme button clicking', async () => {
-    expect(true).toBeTruthy()
+    expect(result.themeData).toEqual({ color: 'dark' })
   })
 })
