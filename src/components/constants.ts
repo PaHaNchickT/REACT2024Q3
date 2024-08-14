9d60c1f11d265e9173e5e4a2d9e3b1723f827dcb
Pavel <you@example.com>
2024-08-14 23:12:14 +0300

    feat: implement errorPage and errorBoundary components

--- src/components/constants.ts
+++ src/components/constants.ts
@@ -1,1 +1,7 @@
-export const TEXT_CONTENT = {}
+export const TEXT_CONTENT = {
+  btnErrorHome: 'Home',
+  errorTitle: 'Oops',
+  errorText: 'Something went wrong...',
+  errorPageTitle: '404',
+  errorPageText: 'Not found...',
+}
---
+++ src/components/error-boundary/errorBoundary.tsx
@@ -0,0 +1,30 @@
+import { Component, ErrorInfo, ReactNode } from 'react'
+
+import './errorBoundary.css'
+
+export default class ErrorBoundary extends Component<
+  { children: ReactNode; fallback: ReactNode },
+  { hasError: boolean }
+> {
+  constructor(props: { children: ReactNode; fallback: ReactNode }) {
+    super(props)
+    this.state = { hasError: false }
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true }
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+    console.log(error, errorInfo)
+  }
+
+  render() {
+    if (this.state.hasError) {
+      localStorage.removeItem('paul-saved-value')
+      return this.props.fallback
+    }
+
+    return this.props.children
+  }
+}
