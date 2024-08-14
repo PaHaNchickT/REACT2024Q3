cc10e3c0670a6e54782648a4369df510c1e6952f
Pavel <you@example.com>
2024-08-14 23:09:52 +0300

    feat: implement routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
-import { App } from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import { ErrorPage } from './components/error-page/errorPage.tsx'
 import { Provider } from 'react-redux'
 import store from './services/store.ts'
-import { Results } from './components/results/results.tsx'
+import ErrorBoundary from './components/error-boundary/errorBoundary.tsx'
 
 const router = createBrowserRouter([
   {
-    path: 'films',
-    element: <App />,
+    path: '/',
+    element: <p>Main Page</p>,
     errorElement: <ErrorPage />,
-    children: [
-      {
-        index: true,
-        element: <Results />,
-        errorElement: <ErrorPage />,
-      },
-    ],
   },
   {
-    path: '/',
-    element: <Navigate to="films" />,
-    errorElement: <ErrorPage />,
+    path: 'controlled',
+    element: <p>Controlled</p>,
+  },
+  {
+    path: 'uncontrolled',
+    element: <p>Uncontrolled</p>,
   },
 ])
 
@@ -32,9 +27,11 @@ const root = document.getElementById('root')
 if (root) {
   ReactDOM.createRoot(root).render(
     <React.StrictMode>
-      <Provider store={store}>
-        <RouterProvider router={router} />
-      </Provider>
+      <ErrorBoundary fallback={<ErrorPage />}>
+        <Provider store={store}>
+          <RouterProvider router={router} />
+        </Provider>
+      </ErrorBoundary>
     </React.StrictMode>
   )
 }
