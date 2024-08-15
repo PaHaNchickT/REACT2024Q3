59fa83b7c4e772d5c87325a462be87d5050f26f5
Pavel <you@example.com>
2024-08-15 17:14:42 +0300

    feat: implement contrl and uncontrl base components

---
+++ src/components/controlled-form/controlledForm.tsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+// import { TEXT_CONTENT } from '../constants'
+
+export function ControlledForm() {
+  return (
+    <>
+      <p>Controlled</p>
+      <Link to="/">Main Page</Link>
+    </>
+  )
+}
---
+++ src/components/uncontrolled-form/uncontrolledForm.tsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+// import { TEXT_CONTENT } from '../constants'
+
+export function UncontrolledForm() {
+  return (
+    <>
+      <p>Uncontrolled</p>
+      <Link to="/">Main Page</Link>
+    </>
+  )
+}
