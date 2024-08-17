0adb1362b14276cee6ad818417ffc8d1524f539c
Pavel <you@example.com>
2024-08-18 00:28:33 +0300

    feat: write validation errors text

--- src/services/yupSchema.ts
+++ src/services/yupSchema.ts
@@ -4,20 +4,23 @@ import { imageData } from '../components/types'
 export const schema = yup.object().shape({
   login: yup
     .string()
-    .matches(/^[A-ZА-ЯЁ]{1}[a-zа-яёA-ZА-ЯЁ]+$/, 'First letter capital')
-    .required(),
-  age: yup.number().required().positive().integer(),
-  email: yup.string().email().required(),
+    .matches(/^[A-ZА-ЯЁ]{1}[a-zа-яёA-ZА-ЯЁ]+$/, 'The first letter must be capitalized')
+    .required('This field is required'),
+  age: yup.number().required('This field is required').positive('Age must be greater than zero'),
+  email: yup.string().email('Enter valid e-mail').required('This field is required'),
   passOrig: yup
     .string()
-    .required()
+    .required('This field is required')
     .matches(
       /^(?=.*\d)(?=.*[a-zа-я])(?=.*[A-ZА-Я])(?=.*\W).{6,}$/,
-      '1 number, 1 uppercased letter, 1 lowercased letter, 1 special character, 6 chars min'
+      'The password must be at least 6 characters long and contain: 1 number, 1 uppercased letter, 1 lowercased letter and 1 special character'
     ),
-  passConf: yup.string().oneOf([yup.ref('passOrig')], 'Passwords must match'),
-  sex: yup.string().required(),
-  confirm: yup.boolean().oneOf([true], 'Click ffs'),
+  passConf: yup
+    .string()
+    .required('This field is required')
+    .oneOf([yup.ref('passOrig')], 'Passwords must match'),
+  sex: yup.string().required('This field is required'),
+  confirm: yup.boolean().oneOf([true], 'You must accept Terms and Conditions agreement'),
   image: yup
     .mixed()
     .test(
@@ -31,5 +34,5 @@ export const schema = yup.object().shape({
         )
       }
     ),
-  country: yup.string().required(),
+  country: yup.string().required('This field is required'),
 })
